4880def7fbd47d5e05f8cec3e5e0e4a86b635cfa
Akshaya Muthu <akshaya522@gmail.com>
2019-08-30 10:21:05 +0800

    change to imagebutton

--- app/src/main/java/com/example/bluetooth/MainActivity.java
+++ app/src/main/java/com/example/bluetooth/MainActivity.java
@@ -12,6 +12,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -25,7 +26,7 @@ public class MainActivity extends AppCompatActivity {
 
 
 // button
-    Button btnBluetoothSettings;
+    ImageButton btnBluetoothSettings;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
